b993d310591b69f34559c49bafb116685e421e9e
Marvin Reyes <reyemarv@gmail.com>
2020-02-06 14:21:35 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,18 @@
 class Dog 
   
   def name(dog_name)
-    new_name = 
+    new_name = dog-name
+  end
+  
+  def name
+    new_name
+  end
+  
+  def bark
+    puts "Woof!"
+  end
+  
+end
+
   
   
